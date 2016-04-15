e4d1a1e83d99edac641d1d34731e144ef7f2105c
Ruben Burr <ruben.burr@novatec-gmbh.de>
2016-04-15 11:47:38 +0200

    Initial commit

---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest  {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
